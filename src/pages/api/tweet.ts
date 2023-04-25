60dfbc8d83c14fdbba4ebe9be61f1ff0d33cc945
135Prithvi <prithvi128717@gmail.com>
2023-04-25 22:35:54 +0530

    dixed tweet duplicate issue

--- src/pages/api/tweet.ts
+++ src/pages/api/tweet.ts
@@ -1,12 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { client } from "~/twitter";
+import { client, generatePrompt } from "~/twitter";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   await client.v2.tweet(
-    "VR sports simulations: Users can participate in virtual sports simulations that provide a full-body workout, such as boxing, tennis, or soccer. These simulations can be designed to be both fun and challenging, motivating users to work harder to improve their skills."
+    `${generatePrompt} sports simulations: Users can participate in virtual sports simulations that provide a full-body workout, such as boxing, ${generatePrompt }.`
   );
 
   res.status(200).json({ message: " succesfully tweeted" });
